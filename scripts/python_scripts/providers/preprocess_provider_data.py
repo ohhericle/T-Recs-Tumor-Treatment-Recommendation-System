2f6b9d6cf6c536dc6a3e935d74c8cf90b4ca972e
aksamra-berkeley <aksamra@berkeley.edu>
2023-08-01 07:34:13 +0000

    Bug fixes for medical providers data pipeline.

--- scripts/python_scripts/providers/preprocess_provider_data.py
+++ scripts/python_scripts/providers/preprocess_provider_data.py
@@ -4,7 +4,23 @@ import pandas as pd
 
 
 def get_oncologists(medical_providers: str):
-    providers = pd.read_csv(medical_providers, encoding='unicode_escape', low_memory=False)
+
+    providers = pd.read_csv( 
+                             medical_providers, 
+                             encoding='unicode_escape', 
+                             low_memory=False,
+                             converters={"zip": str} 
+                           )
+
+    # fix the weird characters in the header :)
+
+    cols_list = list(providers.columns)
+    to_rename = cols_list[:2]
+
+    map = {to_rename[0] : 'line',
+           to_rename[1] : 'NPI'}
+
+    providers = providers.rename(columns=map)
 
     # only keep providers that have any level of specialization in oncology
     oncologists = providers[
@@ -15,7 +31,8 @@ def get_oncologists(medical_providers: str):
                     (providers['sec_spec_4'].str.contains('ONCOLOGY')) |
                     (providers['sec_spec_all'].str.contains('ONCOLOGY'))
                  ]
-
+    
+    
     # normalize format of column data 
     oncologists = oncologists.fillna('')
 
@@ -23,17 +40,14 @@ def get_oncologists(medical_providers: str):
                                oncologists['frst_nm'] + ' ' + \
                                oncologists['mid_nm'].replace('', '')
  
-    oncologists['zip'] = oncologists['zip'].str[:5]
-    oncologists['zip'] = oncologists['zip'].apply(lambda x: x.zfill(5)).astype(int)
 
-    oncologists['phn_numbr'] = oncologists['phn_numbr'].replace('', 'missing')
-    
-    # drop duplicates
-    oncologists = oncologists.drop_duplicates(subset='full_name', keep='first')
+    oncologists['zip'] = oncologists['zip'].str[:5]
+    oncologists = oncologists[oncologists['st'] != 'PR']
     
     oncologists['uuid'] = oncologists.apply(lambda _: uuid.uuid4().hex, axis=1)
     oncologists['uuid'] = oncologists['uuid'].astype(str)
-   
+
+
     oncologists.to_csv(
             's3://trecs-data-s3/data/clean_data/oncologists.csv',
             index=False
